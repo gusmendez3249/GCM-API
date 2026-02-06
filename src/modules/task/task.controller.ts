4b292556a2f775f90e81902f7e596f236fe69e26
gusmendez3249 <gustavoangelc2005@email.com>
2026-02-05 19:50:01 -0600

    Configuracion de CRUD de TASK

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('api/task')
@@ -6,7 +6,28 @@ export class TaskController {
   constructor(private taskSvc: TaskService) {}
 
   @Get()
-  public login(): Array<string> {
+  public getTask(): Array<string> {
     return this.taskSvc.listTask();
+  }
+
+  @Get(":id")
+  public getTaskById(@Param("id") id: string) {
+    return this.taskSvc.getTaskById(parseInt(id));
+  }
+
+  @Post()
+  public insertTask(@Body() task: any){
+    return this.taskSvc.insert(task);
+  }
+
+  @Put(":id")
+  public update(@Param("id") id:string, @Body() task: any ){
+    return this.taskSvc.update(parseInt(id),task);
+  }
+
+  @Delete(":id")
+  public delete(@Param("id") id: string){
+    console.log(id);
+    return this.taskSvc.delete(parseInt(id));
   }
 }
--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -6,4 +6,16 @@ export class TaskService{
     listTask(): Array<string>{
         return ["Noe","Gus","Omar"];
     }
+    getTaskById(id: number): string {
+        return `Tarea con el id ${ id }`;
+    }
+    insert(task: any): string {
+        return task;
+    }
+    update(id: number, task: any): string {
+        return task;
+    }
+    delete(id: number): string {
+        return `Tarea ${id} eliminada`;
+    }
 }
