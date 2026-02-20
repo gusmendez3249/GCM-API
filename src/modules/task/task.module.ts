0b3bde7d3521af883a042c4cc7c8417aa330df18
gusmendez3249 <gustavoangelc2005@email.com>
2026-02-19 19:56:20 -0600

    fix: Uso de providers (pg, mysql) para conexión a base de dato 2s

--- src/modules/task/task.module.ts
+++ src/modules/task/task.module.ts
@@ -1,16 +1,12 @@
-import { Module } from "@nestjs/common";
-import { arrayBuffer } from "stream/consumers";
-import { TaskService } from "./task.service";
-import { TaskController } from "./task.controller";
-import { mysqlProvider } from "src/common/providers/mysql.provider";
+import { Module } from '@nestjs/common';
+import { arrayBuffer } from 'stream/consumers';
+import { TaskService } from './task.service';
+import { TaskController } from './task.controller';
+import { mysqlProvider } from 'src/common/providers/mysql.provider';
 
 @Module({
-    controllers:[
-        TaskController
-    ],
-    providers:[
-        TaskService,
-        mysqlProvider[0]
-    ]
+  controllers: [TaskController],
+  providers: [TaskService, ...mysqlProvider],
+  exports: [...mysqlProvider, TaskService],
 })
-export class TaskModule{}
+export class TaskModule {}
